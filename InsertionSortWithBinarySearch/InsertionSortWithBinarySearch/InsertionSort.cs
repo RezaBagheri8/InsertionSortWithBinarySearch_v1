2194a00d74c557f5addbd616ae2563807c8742ba
Reza Bagheri <bagherbagherreza@gmail.com>
2022-08-25 15:35:51 +0430

    Initial Repo

---
+++ InsertionSortWithBinarySearch/InsertionSortWithBinarySearch/InsertionSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSortWithBinarySearch
+{
+    public class InsertionSort
+    {
+        RecursiveFinder finder = new RecursiveFinder();
+        int j = 1;
+        public void Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int index = finder.RecursiveFind(array, 0, j - 1, array[i + 1]);
+                int tempd = array[i + 1];
+                if (index != i + 1)
+                {
+                    for (int k = i; k >= index; k--)
+                    {
+                        array[k + 1] = array[k];
+                    }
+                }
+                array[index] = tempd;
+                j++;
+            }
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ InsertionSortWithBinarySearch/InsertionSortWithBinarySearch/Program.cs
@@ -0,0 +1,7 @@
+﻿using InsertionSortWithBinarySearch;
+
+int[] array = { 4, 6, 0, 1, 9, 7, 10, -6, 6 };
+
+
+InsertionSort x = new InsertionSort();
+x.Sort(array);
